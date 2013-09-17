bc52689e581ab44e181203b8a59c425d43d3bed0
Cillian Murphy <spacill@murphnduff.org>
2013-09-17 09:49:48 -0700

    Fixed several bugs and implemented MainTest.

--- NumberParsing/src/Main.java
+++ NumberParsing/src/Main.java
@@ -1,54 +1,57 @@
 import java.util.Scanner;
 
-
 public class Main {
-	
+
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
 		int num;
 		System.out.print("Enter a number to convert, 0 to quit: ");
 		num = console.nextInt();
 		while(num > 0) {
-			System.out.println(reduceAndParse(num));
+			System.out.println(parse(num));
 			System.out.print("Enter a number to convert, 0 to quit: ");
 			num = console.nextInt();
 		}
 		console.close();
 		System.out.println("Goodbye!");
 	}
-	
-	public static String reduceAndParse(int n) {
-		String output = reduceNum(n, 0);
-		output = output.trim();
+
+	public static String parse(int n) {
+		String output = reduceAndParse(n);
 		String capOut = output.substring(0, 1).toUpperCase() + output.substring(1) + ".";
 		return capOut;
 	}
-	
-	private static String reduceNum(int n, int power) {
-		if(n < 1000) {
+
+	public static String reduceAndParse(int n) {
+		return reduceNum(n, 0).trim();
+	}
+
+	public static String reduceNum(int n, int power) {
+		if (n < 1000) {
 			return parseNum(n, power);
 		} else {
-			return reduceNum(n/1000, power + 1) + parseNum(n%1000, power);
+			return reduceNum(n / 1000, power + 1) + parseNum(n % 1000, power);
 		}
 	}
-	
-	private static String parseNum(int n, int power) {
+
+	public static String parseNum(int n, int power) {
 		String output = "";
-		if(n % 100 >= 10 && n % 100 < 20) {
+		if (n % 100 >= 10 && n % 100 < 20) {
 			output += parseTeens(n);
+			n /= 100;
 		} else {
-			output = parseOnes(n%10);
+			output = parseOnes(n % 10);
 			n /= 10;
-			output = parseTens(n%10) + output;
+			output = parseTens(n % 10) + output;
 			n /= 10;
-			output = parseHundreds(n%10) + output;
 		}
-		output += powersOfThousand(power);
+		output = parseHundreds(n % 10) + output;
+		if(!output.equals("")) output += powersOfThousand(power);
 		return output;
 	}
-	
-	private static String powersOfThousand(int power) {
-		switch(power) {
+
+	public static String powersOfThousand(int power) {
+		switch (power) {
 		case 0:
 			return "";
 		case 1:
@@ -61,9 +64,9 @@ public class Main {
 			return "Error!";
 		}
 	}
-	
-	private static String parseOnes(int n) {
-		switch(n) {
+
+	public static String parseOnes(int n) {
+		switch (n) {
 		case 9:
 			return " nine";
 		case 8:
@@ -86,11 +89,11 @@ public class Main {
 			return "";
 		default:
 			return " ===There was an error! n = " + n + "=== ";
-		}	
+		}
 	}
-	
-	private static String parseTens(int n) {
-		switch(n) {
+
+	public static String parseTens(int n) {
+		switch (n) {
 		case 9:
 			return " ninety";
 		case 8:
@@ -113,36 +116,37 @@ public class Main {
 			return "";
 		default:
 			return " ===There was an error! n = " + n + "=== ";
-		}	
+		}
 	}
-	
-	private static String parseHundreds(int n) {
-		if(n == 0) return "";
+
+	public static String parseHundreds(int n) {
+		if (n == 0)
+			return "";
 		return parseOnes(n) + " hundred";
 	}
-	
-	private static String parseTeens(int n) {
-		switch(n%100) {
+
+	public static String parseTeens(int n) {
+		switch (n % 100) {
 		case 19:
-			return" nineteen";
+			return " nineteen";
 		case 18:
-			return" eighteen";
+			return " eighteen";
 		case 17:
-			return" seventeen";
+			return " seventeen";
 		case 16:
-			return" sixteen";
+			return " sixteen";
 		case 15:
-			return" fifteen";
+			return " fifteen";
 		case 14:
-			return" fourteen";
+			return " fourteen";
 		case 13:
-			return" thirteen";
+			return " thirteen";
 		case 12:
-			return" twelve";
+			return " twelve";
 		case 11:
-			return" eleven";
+			return " eleven";
 		case 10:
-			return" ten";
+			return " ten";
 		default:
 			return "Error in parseTeens()!";
 		}
--- NumberParsing/src/MainTest.java
+++ NumberParsing/src/MainTest.java
@@ -6,8 +6,59 @@ import org.junit.Test;
 public class MainTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void parseTest() {
+		assertEquals("five", Main.reduceAndParse(5));
+		assertEquals("fifteen", Main.reduceAndParse(15));
+		assertEquals("fifty", Main.reduceAndParse(50));
+		assertEquals("fifty five", Main.reduceAndParse(55));
+		assertEquals("five hundred fifteen", Main.reduceAndParse(515));
+		assertEquals("five hundred fifty five", Main.reduceAndParse(555));
+		assertEquals("five million fifty thousand five hundred fifteen", Main.reduceAndParse(5050515));
+		assertEquals("fifty million", Main.reduceAndParse(50000000));
+		assertEquals("one billion fifteen million fifty five thousand five hundred fifteen", Main.reduceAndParse(1015055515));
+		assertEquals("one billion five hundred fifteen million five hundred fifty five thousand five hundred five", Main.reduceAndParse(1515555505));
+		assertEquals("two billion", Main.reduceAndParse(2000000000));
+		assertEquals("two billion one hundred fourty seven million four hundred eighty three thousand six hundred fourty seven", Main.reduceAndParse(2147483647)); //MAXIMUM INTEGER!
 	}
-
+	
+	@Test
+	public void TeensParseTest() {
+		int[] teens = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+		String[] stringTeens = {" ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen", " seventeen", " eighteen", " nineteen"};
+		for(int i = 0; i < 10; i++) {
+			assertEquals(stringTeens[i], Main.parseTeens(teens[i]));
+		}
+	}
+	
+	@Test
+	public void OnesParseTest() {
+		String[] ones = {"", " one", " two", " three", " four", " five", " six", " seven", " eight", " nine"};
+		for(int i = 0; i < 10; i++) {
+			assertEquals(ones[i], Main.parseOnes(i));
+		}
+	}
+	
+	@Test
+	public void TensParseTest() {
+		String[] tens = {"", " ten", " twenty", " thirty", " fourty", " fifty", " sixty", " seventy", " eighty", " ninety"};
+		for(int i = 0; i < 10; i++) {
+			assertEquals(tens[i], Main.parseTens(i));
+		}
+	}
+	
+	@Test
+	public void HundredsParseTest() {
+		String[] hundreds = {"", " one hundred", " two hundred", " three hundred", " four hundred", " five hundred", " six hundred", " seven hundred", " eight hundred", " nine hundred"};
+		for(int i = 0; i < 10; i++) {
+			assertEquals(hundreds[i], Main.parseHundreds(i));
+		}
+	}
+	
+	@Test
+	public void ThousandsPlaceTest() {
+		String[] thousandsPlaces = {" one", " one thousand", " one million", " one billion"};
+		for(int i = 0; i < 4; i++) {
+			assertEquals(thousandsPlaces[i], Main.reduceNum(1, i));
+		}
+	}
 }
